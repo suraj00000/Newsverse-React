6956a8d970c3f6c843815b5b967346bee405085e
Suraj kushwaha <skk74143@gmail.com>
2022-01-27 12:06:03 +0530

    Created project

---
+++ src/Components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import NewsItem from './NewsItem';
+
+export class News extends Component {
+  render() {
+    return <div>
+        this is a news 
+        <NewsItem/>
+    </div>;
+  }
+}
+
+export default News;
---
+++ src/Components/NewsItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class NewsItem extends Component {
+  render() {
+    return <div> News item</div>;
+  }
+}
+
+export default NewsItem;
