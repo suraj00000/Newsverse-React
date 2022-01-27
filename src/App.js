3d91dd124cf60eab63eadd66cc3ebb037fae147e
Suraj kushwaha <skk74143@gmail.com>
2022-01-28 01:23:52 +0530

    Added catagory feature

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React, { Component } from 'react';
 import NavBar from './Components/NavBar';
 import News from './Components/News';
 
+
+
 export default class App extends Component {
   c = 'jhon';
   render() {
     return (
       <div>
       <NavBar/>
-      <News pageSize={5}/>
+      <News pageSize={5} category="science" />
       </div>
     );
   }
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
+import PropTypes from 'prop-types';
+
 
 export class News extends Component {
   constructor() {
@@ -12,8 +14,23 @@ export class News extends Component {
     }
   }
 
+  static defaultProps={
+    country:'in',
+    pageSize:5,
+    category:'general'
+  }
+  
+  static propTypes={
+    country:PropTypes.string,
+    page:PropTypes.number,
+    category:PropTypes.string
+  
+
+  }
+
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=ffaf0e05bdd143ce8a9b76014d4a3c6d&page=1&page=1&pageSize=${this.props.pageSize}`;
+    console.log(this.props.category);
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ffaf0e05bdd143ce8a9b76014d4a3c6d&page=1&pageSize=${this.props.pageSize}`;    
     this.setState({loading:true});
     let data = await fetch(url);
     let pasrsedData = await data.json();
@@ -22,7 +39,7 @@ export class News extends Component {
 
   handlePreviouseClick = async () => {
     console.log('Previous');
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=ffaf0e05bdd143ce8a9b76014d4a3c6d&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ffaf0e05bdd143ce8a9b76014d4a3c6d&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
     this.setState({loading:true});
     let data = await fetch(url);
     let pasrsedData = await data.json();
@@ -36,7 +53,7 @@ export class News extends Component {
     console.log("nexttt");
     if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {      
       console.log("next");
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=ffaf0e05bdd143ce8a9b76014d4a3c6d&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ffaf0e05bdd143ce8a9b76014d4a3c6d&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
       this.setState({loading:true});
       let data = await fetch(url);
       let pasrsedData = await data.json();
