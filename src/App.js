16d6b20e9917610f057eb652c9217cb6235399e4
Suraj kushwaha <skk74143@gmail.com>
2022-01-28 14:05:52 +0530

    Added React Router and create individual links for every category

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 import React, { Component } from 'react';
 import NavBar from './Components/NavBar';
 import News from './Components/News';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
 export default class App extends Component {
-  c = 'jhon';
   render() {
     return (
       <div>
-      <NavBar/>
-      <News pageSize={5} category="science" />
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<News key="general" pageSize={5} category="general" />} />
+            <Route exact path="/science" element={<News key="science" pageSize={5} category="science" />} />
+            <Route exact path="/business" element={<News key="business" pageSize={5} category="business" />} />
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={5} category="entertainment" />} />
+            <Route exact path="/health" element={<News key="health" pageSize={5} category="health" />} />
+            <Route exact path="/sports" element={<News key="sports" pageSize={5} category="sports" />} />
+            <Route exact path="/technology" element={<News key="technology" pageSize={5} category="technology" />} />
+          </Routes>
+        </Router>
       </div>
     );
   }
